cfc876212224b3c0c68af7cfad6dff005c986f79
Chantel Phillips <chaphi90@Chantels-MacBook-Air.local>
2024-08-23 11:54:20 -0400

    Added HeartList

--- i-heart-react/src/App.js
+++ i-heart-react/src/App.js
@@ -1,28 +1,16 @@
-import HeartLogo from './heart.svg'; // display svg file
+import HeartsList from './components/HeartsList';
+import Header from './components/Header';
 import './App.css';  // css file to style page
 
-const message = 'cool cud';
 
-const Heart = (props) => {
-    return (
-        <div className='heart'>
-            <img className='heart-img' src={HeartLogo} alt='heart' />
-            <p className='heart-message'>{props.msg}</p>
-        </div>
-    )
-}
 
-const Header = () => {
-    return (
-        <div className='App-header'>I ❤️ React</div>
-    );
-}
+
 
 function App() {  // stateless functional component
   return (
         <>
             <Header />;
-            <Heart msg={message} />;
+            <HeartsList />;
         </>
     );
 }
